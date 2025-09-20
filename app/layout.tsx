ad470f74aab598e88a543eae6b6260b87b6d5127
Muneeb-Aslam <muneebbaslam@gmail.com>
2025-09-20 19:03:30 +0500

    main files

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,31 +7,31 @@ import ReduxProvider from "./provider";
 // import { PageLoading } from "./components/PageLoading";
 
 const poppins = Poppins({
-   subsets: ["latin"],
-   weight: "400",
+  subsets: ["latin"],
+  weight: "400",
 });
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function RootLayout({
-   children,
+  children,
 }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en">
-         <body className={poppins.className}>
-            <ReduxProvider>
-               <SessionProvider>
-                  <main className="bg-white w-full min-h-screen flex flex-col justify-start items-center">
-                     {children}
-                     <div id="backdrop-hook" />
-                     <div id="overlay" />
-                  </main>
-                  <Toaster />
-               </SessionProvider>
-            </ReduxProvider>
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <ReduxProvider>
+          <SessionProvider>
+            <main className="bg-white w-full min-h-screen flex flex-col justify-start items-center">
+              {children}
+              <div id="backdrop-hook" />
+              <div id="overlay" />
+            </main>
+            <Toaster />
+          </SessionProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
 }
